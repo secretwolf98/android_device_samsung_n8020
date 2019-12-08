b450d64fdf8189af299c7fe2b07232409b978945
Secretwolf98 <jordanluders@gmail.com>
2019-12-08 06:17:39 -0600

    Update the build fingerprint.

--- lineage.mk
+++ lineage.mk
@@ -7,7 +7,7 @@ $(call inherit-product, device/samsung/n8020/n8020.mk)
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
@@ -21,5 +21,5 @@ PRODUCT_MANUFACTURER := samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME=p4noteltexx \
 	TARGET_DEVICE=p4notelte \
-	BUILD_FINGERPRINT="samsung/p4noteltexx/p4notelte:4.4.2/KOT49H/N8020XXUDOL1:user/release-keys" \
-	PRIVATE_BUILD_DESC="p4noteltexx-user 4.4.2 KOT49H N8020XXUDOL1 release-keys"
+	BUILD_FINGERPRINT="lineage/p4noteltexx/p4notelte:7.1.2/NJH47F/ea585c7dd6:userdebug/test-keys" \
+	PRIVATE_BUILD_DESC="p4noteltexx-userdebug 7.1.2 NJH47F ea585c7dd6 test-keys"
